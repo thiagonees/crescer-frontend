30cab7c92063ee8978291d9f332614bbcec5147d
thiagonees <thiagoalves.devp@gmail.com>
2024-11-27 10:40:56 -0300

    💄 Update contact button and add features

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,52 @@
-export default function ContatcPage() {
-    return (
-      <div>
-        <h1>Bem-vindo ao Next.js App Router</h1>
+// pages/contact.tsx
+import Image from 'next/image';
+import styles from './contact.module.css'; // CSS Modules
+
+export default function ContactPage() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Entre em Contato</h1>
+
+      <div className={styles.icons}>
+        <a
+          href="https://wa.me/SeuNumeroAqui"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.iconButton}
+        >
+          <Image
+            src="/whatsapp.svg"
+            alt="Next.js logo"
+            width={40}
+            height={40}
+            priority
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/crescer.edi?igsh=cjU4NTdpbzZ6YTY5"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.iconButton}
+        >
+          <Image
+            src="/instagram.svg"
+            alt="Next.js logo"
+            width={40}
+            height={40}
+            priority
+          />
+        </a>
       </div>
-    );
-  }
+
+      <div className={styles.mapContainer}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=..."
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          loading="lazy"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
